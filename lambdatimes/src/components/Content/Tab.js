861593c12da79f58809c74b73a1ccc6fa4386e54
Kathy Cuevas <meetnerdykat@gmail.com>
2019-06-03 06:51:33 -0400

    got tabs and cards to appear on the screen, completed changeSelected and filterCards functions, cards are now filterable.

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
 const Tab = props => {
-  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
-      if they match, the className should be: 'tab active-tab', 
-      if it is not it should just be 'tab'*/
+  /* Replace this dummy click handler function with your selectTabHandler 
+  function from props you'll need to pass the `tab` in as an argument to this handler. */
+  const onClickHandler = () => {
+    props.selectTabHandler(props.tab);
+  };
+
   return (
+    /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
+      if they match, the className should be: 'tab active-tab', 
+      if it is not it should just be 'tab'
+  */
     <div
-      className={''}
-      onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
-      }}
+      className={`${
+        props.tab === props.selectedTab ? 'tab active-tab' : 'tab'
+      }`}
+      onClick={onClickHandler}
     >
       {props.tab.toUpperCase()}
     </div>
